0b4b67152cf16ef1ddb3ca482aec98f170106729
6542javed <javedansari6542@gmail.com>
2017-08-06 12:16:45 +0530

    Update Print numbers from 1 to n which are divisible by 7.c

--- Programs upto  6 August/Print numbers from 1 to n which are divisible by 7.c
+++ Programs upto  6 August/Print numbers from 1 to n which are divisible by 7.c
@@ -3,18 +3,16 @@
 int main()
 {
 	int k,n,r;
-  printf("\n Enter the value of n");
-  scanf("%d",&n);
-  for(k=1;k<=n;k++){
-    r = k%7;
-    if(r!=0){
-      printf("%d is not divisible by 7 \n ", k);
-    }else{
-      printf("\n %d is divisible by 7 \n", k);
-    }
-  }
+	printf("\n Enter the value of n: ");
+	scanf("%d",&n);
+	for(k=1;k<=n;k++){
+		r = k%7;
+		if(r!=0){
+			printf("%d is not divisible by 7 \n", k);
+		}else{
+			printf(" %d is divisible by 7 \n", k);
+		}
+	}
 	getch();
 	return 0;
 }
-
-
