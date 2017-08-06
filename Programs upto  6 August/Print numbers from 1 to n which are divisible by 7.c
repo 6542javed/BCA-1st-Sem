bf319ec247c944a21c7a4d91474394d8528f88d2
6542javed <javedansari6542@gmail.com>
2017-08-06 11:34:20 +0530

    Create Print numbers from 1 to n which are divisible by 7.c

---
+++ Programs upto  6 August/Print numbers from 1 to n which are divisible by 7.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int k,n,r;
+  printf("\n Enter the value of n");
+  scanf("%d",&n);
+  for(k=1;k<=n;k++){
+    r = k%7;
+    if(r!=0){
+      printf("%d is not divisible by 7 \n ", r);
+    }else{
+      printf("\n %d is divisible by 7 \n", r);
+    }
+  }
+	getch();
+	return 0;
+}
+
+
